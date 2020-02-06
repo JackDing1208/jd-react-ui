db3bb9a86e2e65e905955864838f49dae2cfed2f
JackDing <361164217@qq.com>
2020-02-06 18:41:50 +0800

    webpack基本配置

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path= require("path")
+const HtmlWebpackPlugin = require( "html-webpack-plugin")
+
+module.exports={
+  mode:"development",
+  //设置入口文件,路径最好加./
+  entry:{
+    index:"./lib/index.ts"
+  },
+  //设置转译后文件输出的文件夹
+  output:{
+    path:path.resolve(__dirname,"dist/lib"),                 //配合node使用绝对路径
+    library:"JD",                                                        //库的名字
+    libraryTarget:"umd"                                                  //输出类型，umd兼容性最好
+  },
+  //根据不同类型的文件用不同的loader进行转译
+  module:{
+    rules:[
+      {
+        test:/\.tsx?$/,    //通过正则检查文件类型
+        loader:"awesome-typescript-loader"
+      }
+    ]
+  },
+  //配置插件
+  plugins: [
+     //目标html自动关联生成的js
+     new HtmlWebpackPlugin({
+       title:"配置测试",
+       template: "index.html",
+     })
+  ]
+}
