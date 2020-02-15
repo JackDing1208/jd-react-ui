a4d5549f712c835181e146addd7fe5c94eb7e60f
JackDing <361164217@qq.com>
2020-02-15 23:15:46 +0800

    补充babel依赖

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Button from "./Button";
+// import Button from "./Button";
 import Icon from "./Icon";
 
 
 const app = document.getElementById("app");
 
-ReactDOM.render(<Icon name={"baidu"}/>, app);
+ReactDOM.render(<Icon name={"hehe"}/>, app);
