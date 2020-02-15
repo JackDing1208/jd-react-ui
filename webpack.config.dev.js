6cc04a4ce5d7975454a9d99d3bdadb251d38070d
JackDing <361164217@qq.com>
2020-02-15 21:47:16 +0800

    webpack分成prod和dev环境分开配置

---
+++ webpack.config.dev.js
@@ -0,0 +1,12 @@
+const base = require('./webpack.config')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = Object.assign({}, base, {
+  mode: 'development',
+  //配置其他插件
+  plugins: [
+    //自动关联HTML用的插件
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
+})
