65c3bb32553dd66733b89c06e2f2c1865ed7cc79
JackDing <361164217@qq.com>
2020-02-17 16:52:19 +0800

    完善测试用例

--- lib/icon/__test__/icon.unit.tsx
+++ lib/icon/__test__/icon.unit.tsx
@@ -1,10 +1,22 @@
-import * as renderer from 'react-test-renderer'
-import * as React from 'react'
-import Icon from '../Icon'
+import * as renderer from "react-test-renderer";
+import * as React from "react";
+import {mount} from "enzyme";
+import Icon from "../Icon";
 
-describe('Icon', () => {
-  it('can render', () => {
-    const json = renderer.create(<Icon name={"react"}/>).toJSON()
-    expect(json).toMatchSnapshot()
-  })
-})
+describe("Icon", () => {
+  it("can render", () => {
+    const json = renderer.create(<Icon name={"react"}/>).toJSON();
+    expect(json).toMatchSnapshot();
+  });
+  it("icon name is right", () => {
+    const json = renderer.create(<Icon name={"react"}/>).toJSON();
+    const iconName = (json as any).children[0].props.xlinkHref
+    expect(iconName).toEqual("#react");
+  });
+  it("has onClick", () => {
+    const fn = jest.fn();
+    const component = mount(<Icon name="react" onClick={fn}/>);
+    component.find("svg").simulate("click");
+    expect(fn).toBeCalled();
+  });
+});
