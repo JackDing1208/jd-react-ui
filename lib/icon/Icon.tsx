6929275e6c9462ff61083ecb439acc296f88337d
JackDing <361164217@qq.com>
2020-02-18 12:23:19 +0800

    svg添加size属性

--- lib/icon/Icon.tsx
+++ lib/icon/Icon.tsx
@@ -4,15 +4,19 @@ import classes from "../helper/className"
 import "./importAllIcons"
 // import "./svg/react.svg"
 
-interface Props extends React.SVGAttributes<SVGElement>{   //继承svg标签属性
-  name: string
+interface Props extends React.SVGAttributes<SVGElement>{   //继承svg标签自带属性
+  name: string,
+  size?: string
 }
 
 
 const Icon: React.FunctionComponent<Props> = (props) => {
-  const {name,className,...rest}=props
+  const {name,className,size,...rest}=props
   return (
-      <svg className={classes("jd-icon",className)} {...rest}>
+      <svg
+        className={classes("jd-icon",className)}
+        style={{width:size,height:size}}
+        {...rest}>
         <use xlinkHref={`#${name}`}/>
       </svg>
   );
--- lib/index.tsx
+++ lib/index.tsx
@@ -6,6 +6,6 @@ import Icon from "./icon/Icon";
 
 const root = document.getElementById("root");
 
-ReactDOM.render(<Icon className={"hehe"} name={"javascript"} onClick={(e) => {
+ReactDOM.render(<Icon className={"hehe"} size={"60px"} name={"react"} onClick={(e) => {
   console.log(e.currentTarget);
 }}/>, root);
