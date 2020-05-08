26fe8c41aedd0e3181f79c047cf983c9b55f7fa3
JackDing <361164217@qq.com>
2020-05-08 23:29:50 +0800

    新增button组件

--- lib/button/Button.tsx
+++ lib/button/Button.tsx
@@ -1,20 +1,37 @@
 import React from "react";
+import classes from "../helper/className";
+import "./style.scss";
+import {Icon} from "../index";
 
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   color?: string
-  onClick: React.MouseEventHandler
+  icon?: string
+  type?: string
+  shape?: string
 }
 
 const Button: React.FunctionComponent<Props> = (props) => {
-  const {onClick, ...rest} = props;
+  const {onClick, className, icon, type, shape, ...rest} = props;
   return (
-    <div
-      className={"jd-button"}
-      onClick={onClick} {...rest}>
-      {props.children}
+    <div className={classes("jd-button", className, shape, type)}
+         onClick={onClick} {...rest}
+         style={{...props.style}}
+    >
+      {icon && <Icon name={icon} size={"1em"}/>}
+      {props.children &&
+      <div className={"jd-button-text"} style={{marginLeft: icon ? "0.5em" : "0"}}>
+        {props.children}
+      </div>
+      }
     </div>
   );
+};
+
+Button.defaultProps = {
+  color: "#1890ff",
+  type: "light",
+  shape: "square"
 };
 
 
--- lib/button/button.example.tsx
+++ lib/button/button.example.tsx
@@ -3,9 +3,15 @@ import Button from "./Button";
 
 const ButtonExample: React.FunctionComponent = () => {
   return (
-    <div>
-      <Button onClick={(e)=>{console.log(e)}}>{"点我"}</Button>
-      </div>
+    <div style={{border:"1px solid red",height:"100px"}}>
+      <Button onClick={(e) => {console.log(e);}}>{"点我"}</Button>
+      <Button style={{marginLeft:"10px"}} icon={"wechat"} shape={"round"}>{"点我"}</Button>
+      <Button icon={"react"} shape={"circle"}/>
+
+      <Button type={"dark"}>{"点我"}</Button>
+      <Button type={"dark"} icon={"qq"} shape={"round"}>{"点我"}</Button>
+      <Button type={"dark"} icon={"user"} shape={"circle"}/>
+    </div>
   );
 };
 
