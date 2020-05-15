b89830ebb26f8a099758e9cb90da8a2b1610b642
jack <361164217@qq.com>
2020-05-15 09:56:59 +0800

    滚动下拉刷新

--- lib/scroll/Scroll.tsx
+++ lib/scroll/Scroll.tsx
@@ -1,25 +1,76 @@
 import * as React from "react"
+import {useState, useRef} from "react"
 import classes from '../helper/className';
 import scrollBarWidth from "./scrollBarWidth"
 
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
-
+  barVisible?: boolean
+  onPull?: () => void
 }
 
 
 const Scroll: React.FunctionComponent<Props> = (props) => {
-  const {children, className, ...rest} = props
+  const {children, className, barVisible, ...rest} = props
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [translateY, _setTranslateY] = useState(0);
+  const setTranslateY = (y: number) => {
+    if (y < 0) {y = 0;} else if (y > 100) {y = 100;}
+    _setTranslateY(y);
+  };
+  const lastYRef = useRef(0);
+  const moveCount = useRef(0);
+  const isPulling = useRef(false);
+
+  const onTouchStart: React.TouchEventHandler = (e) => {
+    const scrollTop = containerRef.current!.scrollTop;
+    if (scrollTop !== 0) {return;}
+    isPulling.current = true;
+    lastYRef.current = e.touches[0].clientY;
+    moveCount.current = 0;
+  };
+  const onTouchMove: React.TouchEventHandler = (e) => {
+    const deltaY = e.touches[0].clientY - lastYRef.current;
+    moveCount.current += 1;
+    if (moveCount.current === 1 && deltaY < 0) {
+      isPulling.current = false;
+      return;
+    }
+    if (!isPulling.current) {return;}
+    setTranslateY(translateY + deltaY);
+    console.log(translateY + deltaY);
+    lastYRef.current = e.touches[0].clientY;
+  };
+  const onTouchEnd: React.TouchEventHandler = () => {
+    if (isPulling.current) {
+      setTranslateY(0);
+      props.onPull && props.onPull();
+      isPulling.current = false;
+    }
+  };
+
 
   return (
     <div className={classes("jd-scroll", className)} {...rest}>
-      <div className={"jd-scroll-view"} style={{right: -scrollBarWidth()}}>
+      <div className={classes("jd-scroll-view", barVisible ? "" : "hideBar")}
+           style={{
+             right: barVisible ? "0px" : -scrollBarWidth(),
+             transform: `translateY(${translateY}px)`
+           }}
+           ref={containerRef}
+           onTouchMove={onTouchMove}
+           onTouchStart={onTouchStart}
+           onTouchEnd={onTouchEnd}
+      >
         {children}
       </div>
     </div>
   )
-
-
 }
+
+Scroll.defaultProps = {
+  barVisible: true
+}
+
 
 export default Scroll
--- lib/scroll/scroll.example.tsx
+++ lib/scroll/scroll.example.tsx
@@ -8,13 +8,23 @@ const ScrollExample: React.FunctionComponent = () => {
   return (
     <div>
       <p className={"example-title"}>组件展示</p>
-      <Scroll style={{height:"400px",width:"400px",border:"1px solid red"}}>
-        <div style={{height:"100px",background:"pink"}}/>
-        <div style={{height:"100px",background:"yellow"}}/>
-        <div style={{height:"100px",background:"orange"}}/>
-        <div style={{height:"100px",background:"blue"}}/>
-        <div style={{height:"100px",background:"bisque"}}/>
-        <div style={{height:"100px",background:"aqua"}}/>
+      移动端使用，支持隐藏滚动条与下拉刷新
+      <Scroll style={{height: "640px", width: "360px"}}
+              barVisible={false}
+              onPull={() => {console.log("下拉回调")}}
+      >
+        <div style={{height: "100px", background: "pink"}}/>
+        <div style={{height: "100px", background: "yellow"}}/>
+        <div style={{height: "100px", background: "orange"}}/>
+        <div style={{height: "100px", background: "blue"}}/>
+        <div style={{height: "100px", background: "bisque"}}/>
+        <div style={{height: "100px", background: "aqua"}}/>
+        <div style={{height: "100px", background: "pink"}}/>
+        <div style={{height: "100px", background: "yellow"}}/>
+        <div style={{height: "100px", background: "orange"}}/>
+        <div style={{height: "100px", background: "blue"}}/>
+        <div style={{height: "100px", background: "bisque"}}/>
+        <div style={{height: "100px", background: "aqua"}}/>
       </Scroll>
       <p className={"example-title"}>示例代码</p>
       <Code code={require('!!raw-loader!./scroll.example.tsx').default}/>
